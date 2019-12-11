8356b86c744eb3635bb4b863f5a979c1fc6248dc
Your Name <you@example.com>
2019-12-11 20:27:51 -0300

    diciembre prueba

--- funciones/hacerRegistro.php
+++ funciones/hacerRegistro.php
@@ -16,8 +16,8 @@ $consulta =$objetoAccesoDato->RetornarConsulta("select nombre from usuario");
 $consulta->execute();
 $datos=$consulta->fetchall(PDO::FETCH_ASSOC);
 //var_dump($datos[0]['nombre']);
-var_dump($datos);
-die();
+//var_dump($datos);
+
 
 foreach ($datos as $usuario) 
 	{
@@ -34,6 +34,9 @@ if(isset($datos[0]['nombre']))
 	{
 	$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
 	$select="INSERT INTO usuario( nombre, clave) VALUES ('$miObjeto->nombre','$miObjeto->contraseña')";
+
+	var_dump($select)
+	die();
 	$consulta =$objetoAccesoDato->RetornarConsulta($select);
 	$consulta->execute();
 }
