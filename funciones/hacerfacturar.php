3e62e7e9594f5670326236102c9d2a2063f7caff
Your Name <you@example.com>
2019-12-18 15:46:57 -0300

    acceso a base

--- funciones/hacerfacturar.php
+++ funciones/hacerfacturar.php
@@ -7,66 +7,75 @@
   
   date_default_timezone_set('America/Argentina/Buenos_Aires');
   $horaSalida = mktime(); 
-  $checkPatente = $_GET['patente'];
-  if (empty($checkPatente)) 
-  {
-    header("Location: ../paginas/facturarVehiculo.php?error=campovacio");
-    exit();
-  }
-  else
-  {
+  $patente2 = $_GET['patente'];
+  
+  
     $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
         $consulta =$objetoAccesoDato->RetornarConsulta("select * from registrovehiculo");
         $consulta->execute();     
         $datos= $consulta->fetchAll(PDO::FETCH_ASSOC);
         var_dump($datos);
         //die();
-        foreach ($datos as $vehiculo) 
-        {
-      if ($vehiculo['patente'] == $checkPatente) 
-        
-      { 
-        $flagNoExiste = 0;
-        $borrar = true;
-        $informarHora = $vehiculo['horaingreso'];
-        //$horaSalida = strtotime($horaSalida);
-        $diffSegundos = $horaSalida - $vehiculo['horaingreso'];
-        $diffAlternativo = $diffSegundos;
-        while ($diffAlternativo >= 3600) 
-        {     
-          if ($diffAlternativo >= 3600) 
-          {
-            $contadorFraccion++;
-            $diffAlternativo = $diffAlternativo - 3600;
-            
-          }
-          else if ($diffAlternativo >= 1800)
-          {
-            $contadorFraccion++;
-          }         
-        }
-        $resultado = $contadorFraccion * $precioFraccion;
-      }
-    }
-    if ($flagNoExiste == 1) 
-    {
-      header("Location: ../paginas/facturarVehiculo.php?error=noexiste");
-      exit();
-    }
-    else if ($flagNoExiste == 0)
+        foreach ($datos as $registrovehiculo) 
+  {
+    if ($registrovehiculo['patente'] == $patente2)
     {
       
-      // Inserte el vahiculo borrado en la tabla de historicos
-      $insert = "INSERT INTO historicavehiculos (patente, horaIngreso, horaEgreso, montoFacturado) VALUES ('$checkPatente','$informarHora','$horaSalida','$resultado')";    
-      $insertar =$objetoAccesoDato->RetornarConsulta($insert);
-      $insertar->execute();
-      // Borramos el vehiculo facturado de la tabla de estacionados
-      $select = "DELETE FROM vehiculosestacionados WHERE patente = '$checkPatente'";
-      // var_dump($select);
-      // die();
-      $borrar = $objetoAccesoDato->RetornarConsulta($select);
-      $borrar->execute();       
-      header("Location: ../paginas/facturarVehiculo.php?cobrar=".$resultado."&ingreso=".$vehiculo['horaIngreso']."&salida=".$horaSalida."&estadia=".$contadorFraccion."&patente=".$checkPatente);
-    }       
-  }
+        $bandera=1;
+
+        date_default_timezone_set('America/Argentina/Buenos_Aires');
+
+        $horaSalida = mktime();
+
+        $tiempo = $horaSalida - $registrovehiculo['horaingreso'];
+
+        $cobrar = ($tiempo / 60 /60) * $precio;
+    
+
+        $objetoFacturado = new stdClass();
+
+        date_default_timezone_set('America/Argentina/Buenos_Aires');
+
+        $objetoFacturado->Vehiculo = $patente2;
+        $objetoFacturado->fechaEntrada = date("d-m-y H:i",$registrovehiculo['horaingreso']);
+        $objetoFacturado->fechaSalida = date("d-m-y H:i",$horaSalida);
+        $objetoFacturado->importe = $cobrar;
+    
+        
+
+            $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+            $select="INSERT INTO vehiculosfacturados( patente, horaingreso, horasalida,importe) VALUES ('$objetoFacturado->Vehiculo','$objetoFacturado->fechaEntrada','$objetoFacturado->fechaSalida','$objetoFacturado->importe')";
+            $consulta =$objetoAccesoDato->RetornarConsulta($select);
+            $consulta->execute();
+       
+
+            $id=$registrovehiculo['id'];
+            $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+            $select="DELETE FROM `registrovehiculo` WHERE id=$id";
+            $consulta =$objetoAccesoDato->RetornarConsulta($select);
+            $consulta->execute();
+
+
+
+       
+        header("Location:../paginas/pagar.php? &cobrar=".$cobrar."&ingreso=".$registrovehiculo['horaingreso']."&salida=".$horaSalida."&patente=".$patente2);
+        exit();
+      }
+   }
+
+   
+ 
+
+
+if ($bandera==0)
+{
+  header("Location: ../paginas/nopatentenolocalizada.php");
+  exit();
+}
+
+//fclose($archivo);
+
 ?>
+
+
+
