eef950d7c8317795ff3ac04650104f250f294e4b
Your Name <you@example.com>
2019-12-18 18:04:23 -0300

    acceso a base

--- descargarcsv.php
+++ descargarcsv.php
@@ -1,4 +1,64 @@
-<?php  
+<?php
+session_start();
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../favicon.ico">
+
+    <title>Istic2019</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="../css/sticky-footer-navbar.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <!--<link href="../css/floating-labels.css" rel="stylesheet">-->
+
+  </head>
+
+        <style>
+body {
+  background-image: url('../parking.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: 100% 100%;
+}
+</style>
+
+
+      <style>
+   
+    th 
+    {
+      color:white;
+      background-color: DodgerBlue ;
+    }
+    td {color:black;}
+    table,th,td 
+    {
+     border: 3px solid black;
+    text-align: center;
+    }
+
+    </style>
+
+  <body>
+
+    <header>
+    <?php
+        include "../componentes/menu.php";
+    ?>
+    </header>
+
+    <!-- Begin page content -->
+    <main role="main" class="container">
+    	<?php
 include 'accesoadatos.php';
 // output headers so that the file is downloaded rather than displayed
 header('Content-Type: text/csv; charset=utf-8');
@@ -15,7 +75,4 @@ foreach ($datos as $vehiculo) {
 	$array = array(	$vehiculo['patente'],date("d-m-y H:i",$vehiculo['horaingreso']),	date("d-m-y H:i",$vehiculo['horasalida']),$vehiculo['importe']);
 	fputcsv($output, $array,';');
 }
-// $mysqli = new mysqli('localhost', 'root', '', 'estacionamiento');
-// $mysqli->set_charset("utf8");
-// $res = $mysqli->query("SELECT patente,horaIngreso,horaEgreso,montoFacturado FROM table");
-// while($f = $res->mysqli_fetch_array()) fputcsv($output, $row);
+?>
